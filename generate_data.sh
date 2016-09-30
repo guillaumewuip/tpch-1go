b4faba2766536d339d995defee0d219bc160d901
Guillaume Wuip <guillaume@wuips.com>
2016-09-30 09:24:08 +0200

    Show sqlldr logs

--- generate_data.sh
+++ generate_data.sh
@@ -4,7 +4,8 @@ for f in /load_files/*; do
     case "$f" in
         *.ctl)
             echo $f
-            sqlldr system/oracle control="$f" direct=y errors=0
+            sqlldr system/oracle control="$f" direct=y errors=0 log="$f.log"
+            cat "$f.log"
     esac
     echo
 done
